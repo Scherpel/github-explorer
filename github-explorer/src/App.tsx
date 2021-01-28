780fcbce72d79616c74d9e69cf6635f72768c6e1
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-01-28 13:13:51 -0300

    Creating gloabal css file

--- github-explorer/src/App.tsx
+++ github-explorer/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+  
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <GlobalStyles>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </GlobalStyles>
+    </>
   );
 }
 
