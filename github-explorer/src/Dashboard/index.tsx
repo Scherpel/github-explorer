604d49934d566d272e64f594dff1de732e9cb8a5
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-01-30 22:34:43 -0300

    add logo ad stylishing the form and button component from Dashboard file

--- github-explorer/src/Dashboard/index.tsx
+++ github-explorer/src/Dashboard/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Title } from './styles'
+import { Title, Form} from './styles'
+import logoImg from '../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
-    return <Title>Dashboard</Title>;
+    return (
+    <>
+        <img src={logoImg} alt="GitExplorer logo"/>
+        <Title>Explore repositórios no Github</Title>
+        <Form>
+            <input placeholder="Digite aqui o nome do repositório"/>
+            <button type="submit">Pesquisar</button>
+        </Form>
+    </>
+    );
 }
 
 export default Dashboard;
