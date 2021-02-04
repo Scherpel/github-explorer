7692f7253683458d8e8090afb433910a492a4e87
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-04 02:05:55 -0300

    Using localStorage to mantian items on page when reloaded

--- github-explorer/src/Dashboard/index.tsx
+++ github-explorer/src/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { Title, Form, Repositories, Error } from './styles'
 import { FiChevronRight } from 'react-icons/fi'
 import logoImg from '../assets/logo.svg'
@@ -18,8 +18,22 @@ const Dashboard: React.FC = () => {
 
     const [newRepo, setNewRepo] = useState('');
     const [inputErro, setInputErro] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>( () => {
+        const storedRepositories = localStorage.getItem('@GithubExplorer:repositories');
 
+        if(storedRepositories) {
+            return JSON.parse(storedRepositories);
+        } else {
+            return [];
+        }
+
+
+    } );
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
+    
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault();
 
