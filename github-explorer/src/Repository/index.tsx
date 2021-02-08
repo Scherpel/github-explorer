095a68e4c1998dd13388d8bf9f9ba88aabbf2e11
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-07 22:41:57 -0300

    Creating new styles page and adding header ad Repository file

--- github-explorer/src/Repository/index.tsx
+++ github-explorer/src/Repository/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import { Header } from './styles';
+import logoImg from '../assets/logo.svg';
+
 
 interface RepositoryParams {
     repository: string
 }
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
-return <h1>Repository: {params.repository}</h1>;
-}
+    return <Header>
+        <img src={logoImg} alt="GitExplorer logo"/>
+        Repository: {params.repository}
+    </Header>;
+
+};
 
 export default Repository;
