f238e9e9f8d41e29f6b3a85bcb9170063be572b4
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-09 00:40:56 -0300

    adding more stylishment to Repository page

--- github-explorer/src/Repository/index.tsx
+++ github-explorer/src/Repository/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import { Header } from './styles';
 import logoImg from '../assets/logo.svg';
-
-
+import { FiChevronLeft } from 'react-icons/fi'
 interface RepositoryParams {
     repository: string
 }
@@ -11,7 +10,11 @@ const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
     return <Header>
         <img src={logoImg} alt="GitExplorer logo"/>
-        Repository: {params.repository}
+        <Link to="/dashboard">
+            <FiChevronLeft size={16}/>
+            Voltar
+        </Link>
+        {/* Repository: {params.repository} */}
     </Header>;
 
 };
