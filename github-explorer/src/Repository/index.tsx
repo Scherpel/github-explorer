624695fa747903f8e1ab55d8ed0e82fb3bc3c6cc
Lucas Scherpel <lucasguarnelli@hotmail.com>
2021-02-13 19:42:53 -0300

    Adding api integration and finishing project (:

--- github-explorer/src/Repository/index.tsx
+++ github-explorer/src/Repository/index.tsx
@@ -1,57 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
+import api from '../services/api';
 import { Header, RepositoryInfo, Issues } from './styles';
 import logoImg from '../assets/logo.svg';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
 interface RepositoryParams {
     repository: string
 }
+interface Repository {
+    full_name: string,
+    description: string,
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string,
+        avatar_url: string,
+    }
+    
+}
+interface Issue {
+    id: number,
+    title: string,
+    html_url: string,
+    user: {
+        login: string
+    },
+}
+
 const Repository: React.FC = () => {
+    const [ repository, setRepository ] = useState<Repository | null>(null);
+    const [ issues, setIssues ] = useState<Issue[]>([]);
     const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then((response) => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then((response) => {
+            setIssues(response.data);
+        })
+    }, [])
     return (
         <>
             <Header>
                 <img src={logoImg} alt="GitExplorer logo"/>
-                <Link to="/dashboard">
+                <Link to="/ ">
                     <FiChevronLeft size={16}/>
                     Voltar
                 </Link>
             </Header>
-            <RepositoryInfo>
-            <header>
-                <img src="https://images.squarespace-cdn.com/content/v1/581e5b6a8419c273288db3e9/1580665601861-JTSHMI6IL3ST58TXK32M/ke17ZwdGBToddI8pDm48kB1kU8bvufUeAQ60mW6QYz9Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyQZC19E8KpwFMajRZknZid4dP1m1zDBXggZaxgAOUvy6nnmrtb1V1wH7EOH7U5amk/Hasham.jpg" />
-                <div>
-                    <strong>Scherpel/github-explorer</strong>
-                    <p>Descrição do repositório</p>
-                </div>
-            </header>
-            <ul>
-                <li>
-                    <strong>1808</strong>
-                    <span>Stars</span>
-                </li>
-                <li>
-                    <strong>48</strong>
-                    <span>Forks</span>
-                </li>
-                <li>
-                    <strong>67</strong>
-                    <span>Issues abertas</span>
-                </li>
-            </ul>
-            </RepositoryInfo>
-            <Issues>
-                <Link to="aa"> 
+            
+            { repository && (
+                <RepositoryInfo>
+                <header>
+                    <img src={repository.owner.avatar_url} />
                     <div>
-                        <strong>skdlsdk</strong>
-                        <p>aaa</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
-                    <FiChevronRight size={20} />
-                </Link>
+                </header>
+                <ul>
+                    <li>
+                        <strong>{repository.stargazers_count}</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>{repository.forks_count}</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>{repository.open_issues_count}</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+                </RepositoryInfo>
+
+                )
+            }
+            <Issues>
+                {issues.map( (issue) => (
+                    <a key={issue.id} href={issue.html_url}> 
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Issues>
         </>
     );
-
 };
 
 export default Repository;
